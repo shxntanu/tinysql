82a6d618372f201045694f6080b9c8f20fb5c3ee
Shantanu <shantanuwable2003@gmail.com>
2024-09-16 21:38:37 +0530

    Add description about meta commands

--- src/meta_commands.h
+++ src/meta_commands.h
@@ -1,6 +1,10 @@
 #include "utils.h"
 #include <string.h>
 
+/**
+ * Non-SQL statements like .exit are called “meta-commands”. They all start with
+ * a dot, so we check for them and handle them in a separate function.
+ */
 typedef enum {
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED_COMMAND
