87dbba0e85023250634ee2206a7ca8b9a275330d
Shantanu <shantanuwable2003@gmail.com>
2024-09-16 20:52:36 +0530

    Refactor main.c to include meta_commands.h and utils.h

--- src/main.c
+++ src/main.c
@@ -1,69 +1,16 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef enum {
-  META_COMMAND_SUCCESS,
-  META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
-
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
-
-typedef struct {
-  StatementType type;
-} Statement;
-
-typedef struct {
-  char *buffer;
-  size_t buffer_length;
-  ssize_t input_length;
-} InputBuffer;
-
-InputBuffer *new_input_buffer() {
-  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
-  input_buffer->buffer = NULL;
-  input_buffer->buffer_length = 0;
-  input_buffer->input_length = 0;
-
-  return input_buffer;
-}
-
-void print_prompt() { printf("db > "); }
-
-void read_input(InputBuffer *input_buffer) {
-  ssize_t bytes_read =
-      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-
-  if (bytes_read <= 0) {
-    printf("Error reading input\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // Ignore trailing newline
-  input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read - 1] = 0;
-}
-
-void close_input_buffer(InputBuffer *input_buffer) {
-  free(input_buffer->buffer);
-  free(input_buffer);
-}
-
-MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
-  if (strcmp(input_buffer->buffer, ".exit") == 0) {
-    close_input_buffer(input_buffer);
-    exit(EXIT_SUCCESS);
-  } else {
-    return META_COMMAND_UNRECOGNIZED_COMMAND;
-  }
-}
+#include "meta_commands.h"
+#include "utils.h"
 
 PrepareResult prepare_statement(InputBuffer *input_buffer,
                                 Statement *statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
+                               &(statement->row_to_insert.id),
+                               &(statement->row_to_insert.username),
+                               &(statement->row_to_insert.email));
+
+    if (args_assigned < 3)
+      return PREPARE_SYNTAX_ERROR;
     statement->type = STATEMENT_INSERT;
     return PREPARE_SUCCESS;
   }
