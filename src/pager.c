46edaf3acbfdb4cbdd229293b487cc3112a43d8a
Shantanu <shantanuwable2003@gmail.com>
2024-09-22 09:19:17 +0530

    define a pager file

---
+++ src/pager.c
@@ -0,0 +1,37 @@
+#include "pager.h"
+
+void get_page(Pager *pager, uint32_t page_num) {
+  if (page_num > TABLE_MAX_PAGES) {
+    printf("Tried to fetch page number out of bounds. %d > %d\n", page_num,
+           TABLE_MAX_PAGES);
+    exit(EXIT_FAILURE);
+  }
+
+  if (pager->pages[page_num] == NULL) {
+    // Cache miss. Allocate memory and load from file.
+    void *page = malloc(PAGE_SIZE);
+    uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+    // We might save a partial page at the end of the file
+    if (pager->file_length % PAGE_SIZE) {
+      num_pages += 1;
+    }
+
+    if (page_num <= num_pages) {
+      lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+      ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+      if (bytes_read == -1) {
+        printf("Error reading file: %d\n", errno);
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    pager->pages[page_num] = page;
+
+    if (page_num >= pager->num_pages) {
+      pager->num_pages = page_num + 1;
+    }
+  }
+
+  return pager->pages[page_num];
+}
---
+++ src/pager.h
@@ -0,0 +1,23 @@
+#ifndef PAGER_H
+#define PAGER_H
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+
+struct Pager_t {
+  int file_descriptor;
+  uint32_t file_length;
+  uint32_t num_pages;
+  void *pages[TABLE_MAX_PAGES];
+};
+typedef struct Pager_t Pager;
+
+void *get_page(Pager *pager, uint32_t page_num);
+
+#endif PAGER_H
